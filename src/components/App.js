b3a437e665d53601eaeffc67054672d7b901fc25
quedacoder <quedacoder@gmail.com>
2021-07-11 18:57:40 -0500

    Wired up Search Bar and console logging changes in terminal

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import SearchBar from './SearchBar'
 
 class App extends React.Component {
+
+    onSearchSubmit = (term) => {
+        console.log(term);
+    }
+
     render() {
         return (
-            <div>
-                <SearchBar />
+            <div className='ui container' style={{ marginTop: '1%' }}>
+                <SearchBar onSubmit={this.onSearchSubmit}/>
             </div>
             
-        )
+        );
     }
 } 
 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,29 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+
+    state = {term: ''}
+
+    onFormSubmit = (event) => {
+
+       //stops the page from refreshing on form submit
+        event.preventDefault();
+
+        //call function passed in from the parent component through props
+        this.props.onSubmit(this.state.term);
+    }
+
     render() {
-        return <div > SearchBar </div>;
+        return (
+            <div className='ui segment'>
+                <form className='ui form' action="" onSubmit={this.onFormSubmit}> 
+                    <div className='field'>
+                        <label>Video Search</label>
+                        <input type='text' value={this.state.term}  onChange={(e) => this.setState({term: e.target.value})}/>
+                    </div>
+                </form>
+            </div>
+        );
     }
 }
 
